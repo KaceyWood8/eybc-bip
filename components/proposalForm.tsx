69c1617d155360a537cee36da77214dcd5d016bb
kaceywood8 <kacey.l.wood@ey.com>
2022-10-20 15:19:33 -0400

    Merged with Master

--- components/proposalForm.tsx
+++ components/proposalForm.tsx
@@ -96,7 +96,7 @@ const ProposalForm = () => {
           <Textarea {...register("motivation")} />
         </FormControl>
 
-        <FormControl isRequired>
+        <FormControl>
           <FormLabel>Specifications</FormLabel>
           <FormHelperText>
             Enter detailed description of the proposal. Feel free to add
